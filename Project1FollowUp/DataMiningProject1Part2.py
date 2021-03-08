14fe1bb558285942a28f6d1551681b7d91927206
JoshSample <joshsample98@gmail.com>
2021-03-08 15:27:46 -0600

    Added shuffle function to shuffle dataset

--- Project1FollowUp/DataMiningProject1Part2.py
+++ Project1FollowUp/DataMiningProject1Part2.py
@@ -92,9 +92,26 @@ def predict_grade(df, probYes, probNo, attrib_dict):
     print(correctness)  
     return new_df
 
+def shuffle(ds):
+    # Shuffle the dataset
+    # then split data into 5 equal parts
+    df = ds.sample(frac = 1)
+    splitsNum = int(len(df) / 5)
+    set1 = df.iloc[0:splitsNum]
+    set2 = df.iloc[splitsNum+1:splitsNum*2]
+    set3 = df.iloc[splitsNum*2+1:splitsNum*3]
+    set4 = df.iloc[splitsNum*3+1:splitsNum*4]
+    set5 = df.iloc[splitsNum*4+1:splitsNum*5]
+    return set1, set2, set3, set4, set5
+
+
+
 if __name__ == "__main__":
-    # Load in the training dataset
-    trainingDataset = pd.read_excel('Training dataset.xlsx', engine='openpyxl')
+    # Load in the whole dataset
+    trainingDataset = pd.read_excel('Full Wine Data.xlsx', engine='openpyxl')
+
+    # Shuffle rows and split into 5 sets
+    (set1, set2, set3, set4, set5) = shuffle(trainingDataset)
 
     # Get the overall totals from the dataset
     (totalYes, totalNo, total, probYes, probNo) = getTotals(trainingDataset)
