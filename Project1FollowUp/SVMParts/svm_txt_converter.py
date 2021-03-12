4c2e13c901d1beb0bf614cfb422fa6941c048e3d
JoshSample <joshsample98@gmail.com>
2021-03-11 19:15:16 -0600

    Fixed an error with shuffle and fixed svm errors

--- Project1FollowUp/SVMParts/svm_txt_converter.py
+++ Project1FollowUp/SVMParts/svm_txt_converter.py
@@ -4,13 +4,13 @@ with open('Project1FollowUp/SVMParts/set5.txt', 'r', encoding='utf-8-sig') as re
         if (words[0] == '0'):
             writer.write('-1  ')
             j = 1
-            for i in words:
-                writer.write(str(j) + ':' + i +' ')
+            for i in range(1, len(words)):
+                writer.write(str(j) + ':' + words[i] +' ')
                 j+=1
         elif (words[0] == '1'):
             writer.write('1  ')
             j = 1
-            for i in words:
-                writer.write(str(j) + ':' + i +' ')
+            for i in range(1, len(words)):
+                writer.write(str(j) + ':' + words[i] +' ')
                 j+=1
             
